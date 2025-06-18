efff659d31297ae9e0d33fdde4dcbdc194ee2c87
Sioood <theodupontpro@gmail.com>
2025-06-18 15:19:53 +0200

    fix: :lipstick: use text center for responsive

--- src/views/ConfirmationView.vue
+++ src/views/ConfirmationView.vue
@@ -17,7 +17,7 @@ const { user } = extractStore(useUserStore())
 </script>
 
 <template>
-  <main class="p-4 h-[calc(100vh-5rem)] flex flex-col items-center justify-center">
+  <main class="p-4 h-[calc(100vh-5rem)] flex flex-col items-center justify-center text-center">
     <h1 class="text-4xl font-bold text-green-600">Merci pour votre commande {{ user?.name }} !</h1>
     <h6 class="text-md text-green-600">Elle va vous être livrée le plus rapidement possible.</h6>
 
