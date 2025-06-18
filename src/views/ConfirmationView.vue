9b7d8388a907836cd69dd7e1314cfad695b2b930
Sioood <theodupontpro@gmail.com>
2025-06-18 15:08:46 +0200

    feat: :sparkles: add return to home button

--- src/views/ConfirmationView.vue
+++ src/views/ConfirmationView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user'
 import { extractStore } from '@/composables/store'
+import UiButton from '@/components/ui/UiButton.vue'
 
 const { user } = extractStore(useUserStore())
 </script>
@@ -9,5 +10,9 @@ const { user } = extractStore(useUserStore())
   <main class="p-4 h-[calc(100vh-5rem)] flex flex-col items-center justify-center">
     <h1 class="text-4xl font-bold text-green-600">Merci pour votre commande {{ user?.name }} !</h1>
     <h6 class="text-md text-green-600">Elle va vous être livrée le plus rapidement possible.</h6>
+
+    <RouterLink to="/" class="mt-4">
+      <UiButton intent="primary" size="medium">Retourner à l'accueil</UiButton>
+    </RouterLink>
   </main>
 </template>
