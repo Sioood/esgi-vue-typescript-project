b845ae43e94d3ad61473fac85d49b0e64e133277
louis-guidez <louisguidez3@gmail.com>
2025-06-18 09:25:40 +0200

    Appel à l'api /api/hello

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const message = ref('');
+
+// Appel à l'API quand le composant est monté
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/hello');
+    const data = await response.json();
+    message.value = data.message;
+  } catch (error) {
+    console.error('Erreur lors de la récupération du message:', error);
+  }
+});
 </script>
 
 <template>
   <main>
-    Hell world
+    {{ message }}
   </main>
 </template>
