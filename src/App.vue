5e50062f336cf289193927b2613bc4a9149ac123
Sioood <theodupontpro@gmail.com>
2025-06-18 16:02:39 +0200

    feat: :dizzy: add view router transition

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,13 @@ watchEffect(() => {
 <template>
   <div>
     <UiNavbar />
-    <RouterView />
+
+    <router-view v-slot="{ Component }">
+      <transition name="slide-fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+
     <Toaster richColors />
 
     <UiDialog
@@ -52,3 +58,19 @@ watchEffect(() => {
     </UiDialog>
   </div>
 </template>
+
+<style>
+.slide-fade-enter-active {
+  transition: all 0.1s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.2s ease-in-out;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(-20px);
+  opacity: 0;
+}
+</style>
