4ce2f4b29d3ed6469ee378e02a9797300b56def7
Sioood <theodupontpro@gmail.com>
2025-06-18 15:33:55 +0200

    refactor: :recycle: move username dialog to app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
 import UiNavbar from '@/components/ui/UiNavbar.vue'
-import { Toaster } from 'vue-sonner'
+import { toast, Toaster } from 'vue-sonner'
 import 'vue-sonner/style.css'
+import UiDialog from '@/components/ui/UiDialog.vue'
+import UsernameForm from '@/components/UsernameForm.vue'
+import { useUserStore } from '@/stores/user'
+import { ref, watchEffect } from 'vue'
+import { extractStore } from '@/composables/store'
+
+const openUsernameForm = ref<boolean>(false)
+
+const { user, setUser } = extractStore(useUserStore())
+
+watchEffect(() => {
+  if (!user.value) {
+    openUsernameForm.value = true
+  }
+})
 </script>
 
 <template>
@@ -9,5 +24,31 @@ import 'vue-sonner/style.css'
     <UiNavbar />
     <RouterView />
     <Toaster richColors />
+
+    <UiDialog
+      :open="openUsernameForm"
+      :closableOnClickOutside="false"
+      @close="openUsernameForm = false"
+      @update:open="openUsernameForm = $event"
+      triggerClass="fixed"
+    >
+      <template v-slot="{ close }">
+        <div class="p-6 flex flex-col gap-6 items-center justify-center">
+          <div class="flex flex-col gap-1 items-center">
+            <h2 class="text-4xl font-bold">Bienvenue sur Burgouzzzzz</h2>
+            <h3>Pour une expérience personnalisée, veuillez renseigner votre nom</h3>
+          </div>
+          <UsernameForm
+            :onSubmit="
+              (username: string) => {
+                setUser({ name: username })
+                toast.success(`Bienvenue ${user!.name}`)
+                close()
+              }
+            "
+          />
+        </div>
+      </template>
+    </UiDialog>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,5 @@
 <template>
   <main class="p-6">
-    <UiDialog
-      :open="openUsernameForm"
-      :closableOnClickOutside="false"
-      @close="openUsernameForm = false"
-      @update:open="openUsernameForm = $event"
-      triggerClass="fixed"
-    >
-      <template v-slot="{ close }">
-        <UsernameForm
-          :onSubmit="
-            (username: string) => {
-              setUser({ name: username })
-              close()
-            }
-          "
-        />
-      </template>
-    </UiDialog>
-
     <div class="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
       <UiMenuCard
         v-for="product in products"
@@ -49,27 +30,16 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, watchEffect } from 'vue'
+import { ref, onMounted } from 'vue'
 import UiMenuCard from '@/components/ui/UiMenuCard.vue'
 import UiSidebar from '@/components/ui/UiSidebar.vue'
-import UiDialog from '@/components/ui/UiDialog.vue'
-import UsernameForm from '@/components/UsernameForm.vue'
-import { useUserStore } from '@/stores/user'
 import { useProductStore } from '@/stores/product'
 import { extractStore } from '@/composables/store'
 import type { Product } from '@/stores/product'
 
 const { products, getProducts } = extractStore(useProductStore())
-const openUsernameForm = ref<boolean>(false)
-const { user, setUser } = extractStore(useUserStore())
 const selectedProduct = ref<Product | null>(null);
 const sidebarVisible = ref(false);
-
-watchEffect(() => {
-  if (!user.value) {
-    openUsernameForm.value = true
-  }
-})
 
 onMounted(async () => {
   await getProducts()
